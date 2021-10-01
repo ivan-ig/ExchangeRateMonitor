25597ab522c8393fca24b99c73d2e845066ef2b8
ivan-ig <ivan_10_93@bk.ru>
2021-10-01 11:15:18 +0300

    Refactoring, adding mock

--- src/test/java/com/github/ivanig/exchangeratemonitor/service/RateCalculatorTest.java
+++ src/test/java/com/github/ivanig/exchangeratemonitor/service/RateCalculatorTest.java
@@ -1,95 +1,96 @@
 package com.github.ivanig.exchangeratemonitor.service;
 
+import com.github.ivanig.exchangeratemonitor.client.OxrFeignClient;
 import com.github.ivanig.exchangeratemonitor.client.model.OxrResponse;
-import org.junit.jupiter.api.Assumptions;
+import com.github.ivanig.exchangeratemonitor.config.OxrProperties;
+import com.github.ivanig.exchangeratemonitor.util.DataUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.time.Instant;
-import java.time.LocalDate;
-import java.time.ZoneId;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.temporal.ChronoUnit;
 
 @SpringBootTest
-//@AutoConfigureMockMvc
 class RateCalculatorTest {
-
-    private OxrResponse today;
-    private OxrResponse yesterday;
 
     @Autowired
     private RateCalculator rateCalculator;
 
-//    @MockBean
-//    private RateCalculator rateCalculator;
+    @Autowired
+    private OxrProperties properties;
 
-//    @Autowired
-//    private MockMvc mvc;
-
-
-//    @Test
-//    public void getActualExchangeRateFromOxrShouldReturnOxrResponse() {
-////        Map<String, BigDecimal> rates = new HashMap<>();
-////        rates.put("USD", new BigDecimal("1"));
-////        rates.put("RUB", new BigDecimal("72.5"));
-////        OxrResponse expectedOxrResponse = new OxrResponse(
-////                "disclaimer", "license", 1_500_000_000L, "USD", rates);
-////
-////        Mockito.when(rateCalculator.getActualExchangeRateFromOxr()).thenReturn(expectedOxrResponse);
-//
-//        today = rateCalculator.getActualExchangeRateFromOxr();
-//        assertNotNull(today);
-//        assertTrue(today.getRates().containsKey("USD"));
-//    }
-
+    @MockBean
+    private OxrFeignClient oxrFeignClient;
 
     @Test
-    public void getActualExchangeRateFromOxrShouldReturnOxrResponse() {
-        today = rateCalculator.getActualExchangeRateFromOxr();
-        assertNotNull(today);
-        assertTrue(today.getRates().containsKey("USD"));
+    public void calculateShouldReturnMinusOne() {
+        OxrResponse todayOxrResponse = DataUtils.getOxrResponseByInstant(Instant.now(), 29L);
+        OxrResponse customDateOxrResponse = DataUtils.getOxrResponseByInstant(
+                Instant.now().minus(Long.parseLong(properties.getNumberOfDaysAgo()), ChronoUnit.DAYS), 33L);
+
+        Mockito.when(oxrFeignClient.getCurrentRate(Mockito.any(), Mockito.any()))
+                .thenReturn(todayOxrResponse);
+        Mockito.when(oxrFeignClient.getCustomDateRate(Mockito.any(), Mockito.any(), Mockito.any()))
+                .thenReturn(customDateOxrResponse);
+
+        final int result = rateCalculator.calculate("RUB", null);
+
+        Assertions.assertEquals(result, -1);
     }
 
     @Test
-    public void getExchangeRateFromOxrByDateShouldReturnOxrResponse() {
-        yesterday = rateCalculator.getExchangeRateFromOxrByDate();
-        assertNotNull(yesterday);
-        assertTrue(yesterday.getRates().containsKey("USD"));
+    public void calculateShouldReturnOne() {
+        OxrResponse todayOxrResponse = DataUtils.getOxrResponseByInstant(Instant.now(), 88L);
+        OxrResponse customDateOxrResponse = DataUtils.getOxrResponseByInstant(
+                Instant.now().minus(Long.parseLong(properties.getNumberOfDaysAgo()), ChronoUnit.DAYS), 49L);
+
+        Mockito.when(oxrFeignClient.getCurrentRate(Mockito.any(), Mockito.any()))
+                .thenReturn(todayOxrResponse);
+        Mockito.when(oxrFeignClient.getCustomDateRate(Mockito.any(), Mockito.any(), Mockito.any()))
+                .thenReturn(customDateOxrResponse);
+
+        final int result = rateCalculator.calculate("RUB", "something");
+
+        Assertions.assertEquals(result, 1);
     }
 
     @Test
-    public void isActualExcRateAfterExcRateByDate() {
-        today = rateCalculator.getActualExchangeRateFromOxr();
-        LocalDate todayDate = Instant.ofEpochSecond(
-                today.getTimestamp()).atZone(ZoneId.of("UTC+00:00")).toLocalDate();
+    public void calculateShouldReturnZero() {
+        OxrResponse todayOxrResponse = DataUtils.getOxrResponseByInstant(Instant.now(), 75L);
+        OxrResponse customDateOxrResponse = DataUtils.getOxrResponseByInstant(
+                Instant.now().minus(Long.parseLong(properties.getNumberOfDaysAgo()), ChronoUnit.DAYS), 75L);
 
-        yesterday = rateCalculator.getExchangeRateFromOxrByDate();
-        LocalDate yesterdayDate = Instant.ofEpochSecond(
-                yesterday.getTimestamp()).atZone(ZoneId.of("UTC+00:00")).toLocalDate();
-        if (!todayDate.isEqual(yesterdayDate)) {
-            assertTrue(todayDate.isAfter(yesterdayDate));
-        } else {
-            Assumptions.assumeTrue(todayDate.isEqual(yesterdayDate),
-                    "No up-to-date exchange rates are available. Exchanges may be closed...");
-        }
-    }
+        Mockito.when(oxrFeignClient.getCurrentRate(Mockito.any(), Mockito.any()))
+                .thenReturn(todayOxrResponse);
+        Mockito.when(oxrFeignClient.getCustomDateRate(Mockito.any(), Mockito.any(), Mockito.any()))
+                .thenReturn(customDateOxrResponse);
 
-    // TODO:
-    //  test GiphySelector +++
-    //  test properties (если нужно)
-    //  test void Controller (если можно и нужно)
+        final int result = rateCalculator.calculate("RUB", null);
 
-
-    @Test
-    public void calculateMethodShouldReturnZero() {
-        assertEquals(rateCalculator.calculate("USD", "USD"), 0);
+        Assertions.assertEquals(result, 0);
     }
 
     @Test
-    public void calculateMethodShouldReturnOneOrMinusOne() {
-        Assumptions.assumeTrue(rateCalculator.calculate("USD", "RUB") == 1);
-        Assumptions.assumeTrue(rateCalculator.calculate("USD", "RUB") == -1);
+    public void rateCalculationBasedOnSameCurrencyShouldReturnZero() {
+        OxrResponse todayOxrResponse = DataUtils.getOxrResponseByInstant(Instant.now(), 88L);
+        OxrResponse customDateOxrResponse = DataUtils.getOxrResponseByInstant(
+                Instant.now().minus(Long.parseLong(properties.getNumberOfDaysAgo()), ChronoUnit.DAYS), 49L);
+
+        Mockito.when(oxrFeignClient.getCurrentRate(Mockito.any(), Mockito.any()))
+                .thenReturn(todayOxrResponse);
+        Mockito.when(oxrFeignClient.getCustomDateRate(Mockito.any(), Mockito.any(), Mockito.any()))
+                .thenReturn(customDateOxrResponse);
+
+        final int result = rateCalculator.calculate("RUB", "RUB");
+        final int resultInUsd = rateCalculator.calculate("USD", null);
+        final int resultInUsdBasedOnWhatever = rateCalculator.calculate("USD", "whatever");
+
+        Assertions.assertEquals(result, 0);
+        Assertions.assertEquals(resultInUsd, 0);
+        Assertions.assertEquals(resultInUsdBasedOnWhatever, 0);
     }
 }
